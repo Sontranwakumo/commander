d0dd2def72daa179afbd42482c83c6249c40e549
Sontranwakumo <sontran@wakumo.vn>
2026-01-22 16:25:38 +0700

    fix: finalize aim scan

--- scripts/aim-scan.sh
+++ scripts/aim-scan.sh
@@ -4,12 +4,21 @@ parse_args "$@"
 auto_load_env "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 export DEV_DB_HOST=$([ "$IS_VPN" = "true" ] && echo "100.70.254.142" || echo "10.123.31.100")
-export PGPASSWORD=$PGPASSWORD_DEV PGHOST=$DEV_DB_HOST PGUSER="local_postgres_user" PGPORT=30838 PGDATABASE="aim-api"
+export PGPASSWORD=$PGPASSWORD_DEV
+export PGHOST=$DEV_DB_HOST
+export PGUSER="local_postgres_user"
+export PGPORT=30838
+export PGDATABASE="aim-api"
 
-[ "$on" = "true" ] && psql <<EOF
-BEGIN; DELETE FROM public.processed_blocks WHERE chain_id = 97; UPDATE public.networks SET is_paused_scan = false WHERE chain_id = 97; COMMIT;
-EOF
+[ "$on" = "true" ] && psql <<SQL
+BEGIN;
+DELETE FROM public.processed_blocks WHERE chain_id = 97;
+UPDATE public.networks SET is_paused_scan = false WHERE chain_id = 97;
+COMMIT;
+SQL
 
-[ "$off" = "true" ] && psql <<EOF
-BEGIN; UPDATE public.networks SET is_paused_scan = true WHERE chain_id = 97; COMMIT;
-EOF
+[ "$off" = "true" ] && psql <<SQL
+BEGIN;
+UPDATE public.networks SET is_paused_scan = true WHERE chain_id = 97;
+COMMIT;
+SQL
