53eb93250531386c30ac2116c95ea0d1444acbc9
Sontranwakumo <sontran@wakumo.vn>
2026-01-23 08:46:43 +0700

    fix: script optimize

--- scripts/aim-scan.sh
+++ scripts/aim-scan.sh
@@ -3,22 +3,44 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/common.sh"
 parse_args "$@"
 auto_load_env "$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-export DEV_DB_HOST=$([ "$IS_VPN" = "true" ] && echo "100.70.254.142" || echo "10.123.31.100")
-export PGPASSWORD=$PGPASSWORD_DEV
-export PGHOST=$DEV_DB_HOST
-export PGUSER="local_postgres_user"
-export PGPORT=30838
-export PGDATABASE="aim-api"
+if [ "$env" = "dev" ]; then
+  export DEV_DB_HOST=$([ "$IS_VPN" = "true" ] && echo "100.70.254.142" || echo "10.123.31.100")
+  export PGPASSWORD=$PGPASSWORD_DEV
+  export PGHOST=$DEV_DB_HOST
+  export PGUSER="local_postgres_user"
+  export PGPORT=30838
+  export PGDATABASE="aim-api"
+elif [ "$env" = "local" ]; then
+  export PGPASSWORD='dhruv314'
+  export PGHOST=$LOCAL_DB_HOST
+  export PGUSER="postgres"
+  export PGPORT=5432
+  export PGDATABASE="aim_api"
+fi
+echo "Updating AIM scan to $env database, DBHost: $PGHOST"
 
-[ "$on" = "true" ] && psql <<SQL
+if [ "$on" = "true" ]; then
+  psql <<SQL
 BEGIN;
 DELETE FROM public.processed_blocks WHERE chain_id = 97;
 UPDATE public.networks SET is_paused_scan = false WHERE chain_id = 97;
 COMMIT;
 SQL
+  echo "AIM scan enabled"
+  psql <<SQL
+SELECT chain_id, is_paused_scan FROM public.networks WHERE chain_id = 97;
+SQL
+fi
 
 [ "$off" = "true" ] && psql <<SQL
 BEGIN;
 UPDATE public.networks SET is_paused_scan = true WHERE chain_id = 97;
 COMMIT;
 SQL
+  echo "AIM scan disabled"
+  psql <<SQL
+SELECT chain_id, is_paused_scan FROM public.networks WHERE chain_id = 97;
+SQL
+fi
+
+exit 0
